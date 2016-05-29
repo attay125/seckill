a40f872a07fc0d657f0d34f3f10d02af2b7fe0eb
moonie <wangchao_moonie@163.com>
2016-05-30 07:54:58 +0800

    service实现

---
+++ src/main/java/org/seckill/exception/RepeatKillException.java
@@ -0,0 +1,15 @@
+package org.seckill.exception;
+
+/**
+ * Created by moonie on 16/5/30.
+ */
+public class RepeatKillException extends SeckillException {
+
+    public RepeatKillException(String message) {
+        super(message);
+    }
+
+    public RepeatKillException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/org/seckill/exception/SeckillCloseException.java
@@ -0,0 +1,15 @@
+package org.seckill.exception;
+
+/**
+ * Created by moonie on 16/5/30.
+ */
+public class SeckillCloseException extends SeckillException {
+
+    public SeckillCloseException(String message) {
+        super(message);
+    }
+
+    public SeckillCloseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
