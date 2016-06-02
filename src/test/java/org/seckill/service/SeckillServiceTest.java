b3bbb782550653a25980ea8315a68d985d44a257
moonie <wangchao_moonie@163.com>
2016-06-02 08:05:57 +0800

    service的单元测试以及集成测试

--- src/test/java/org/seckill/service/SeckillServiceTest.java
+++ src/test/java/org/seckill/service/SeckillServiceTest.java
@@ -2,7 +2,11 @@ package org.seckill.service;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.seckill.dto.Exposer;
+import org.seckill.dto.SeckillExecution;
 import org.seckill.entity.Seckill;
+import org.seckill.exception.RepeatKillException;
+import org.seckill.exception.SeckillCloseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,16 +38,41 @@ public class SeckillServiceTest {
 
     @Test
     public void getById() throws Exception {
-
+        long id = 1L;
+        Seckill seckill = seckillService.getById(id);
+        logger.info("seckill={}",seckill);
     }
 
     @Test
     public void exportSeckillUrl() throws Exception {
-
+        Exposer exposer = seckillService.exportSeckillUrl(1L);
+        logger.info("exposer={}",exposer.getMd5());
+        // 683b5abb140788e3f5808c83e229da81
     }
 
     @Test
     public void executeSeckill() throws Exception {
+        SeckillExecution seckillExecution = seckillService.executeSeckill(1L,15221808911L,"683b5abb140788e3f5808c83e229da81");
+        logger.info("seckillExecution={}",seckillExecution.getSuccessKilled());
+    }
+
+    @Test
+    public void testSeckill() throws Exception{
+        Exposer exposer = seckillService.exportSeckillUrl(1L);
+        logger.info("exposer={}",exposer.getMd5());
+        if(exposer.isExposed()){
+            try {
+                SeckillExecution seckillExecution = seckillService.executeSeckill(1L,15221808913L,exposer.getMd5());
+                logger.info("seckillExecution={}",seckillExecution.getSuccessKilled());
+            }catch (RepeatKillException e){
+                logger.info(e.getMessage());
+            }catch (SeckillCloseException e){
+                logger.info(e.getMessage());
+            }
+        }else{
+            logger.warn("exposer={}",exposer.isExposed());
+        }
+
 
     }
 
